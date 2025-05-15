dbc47153543e53a7a33a1ed9d2ad4168e7a5aaa1
Nilendra Patel <nilendrapatel1317@gmail.com>
2025-05-16 04:45:50 +0530

    backend API added

--- utils/axios.js
+++ utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080', // Spring Boot backend base URL
+  baseURL: process.env.NEXT_PUBLIC_API_URL, // Spring Boot backend base URL
   headers: {
     'Content-Type': 'application/json',
   },
